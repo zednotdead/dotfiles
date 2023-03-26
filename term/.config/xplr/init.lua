6f858d463b73fc00843d795a940cd62c8793e31b
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-03-26 15:03:13 +0200

    add xplr

---
+++ term/.config/xplr/init.lua
@@ -0,0 +1,19 @@
+version = "0.21.1"
+---@diagnostic disable
+local xplr = xplr
+---@diagnostic enable
+
+xplr.config.node_types.directory.meta.icon = ""
+xplr.config.node_types.mime_essence = {
+   application = {
+	   pdf = { meta = { icon = "" }, style = { fg = "Blue" } },
+   }
+}
+xplr.config.node_types.extension = {
+	rs = { meta = { icon = "" } },
+	yaml = { meta = { icon = "" } },
+	yml = { meta = { icon = "" } },
+	toml = { meta = { icon = "" } },
+	lock = { meta = { icon = "" } },
+}
+
