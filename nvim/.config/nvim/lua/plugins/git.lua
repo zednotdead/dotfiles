87cd0dc407a9c10621fbecf4b3847f7f09507f6c
Zbigniew Żołnierowicz <zbigniewzolnierowicz@Zbigniews-MacBook-Pro.local>
2023-05-30 13:48:59 +0200

    fix: disable confirmation for Neogit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -2,6 +2,9 @@ return {
   {
     'TimUntersberger/neogit',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      disable_commit_confirmation = true
+    },
     config = function()
       vim.keymap.set('n', '<leader>gg', "<Cmd>Neogit<CR>", { desc = 'Open Neogit' })
     end
