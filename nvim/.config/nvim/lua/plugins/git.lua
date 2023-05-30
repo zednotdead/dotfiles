a08016cbf3989f8b7a76386bd17efb71fa95be78
Zbigniew Żołnierowicz <zbigniewzolnierowicz@Zbigniews-MacBook-Pro.local>
2023-05-30 13:50:42 +0200

    fix: fix neogit... again

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -5,7 +5,8 @@ return {
     opts = {
       disable_commit_confirmation = true
     },
-    config = function()
+    config = function(_, opts)
+      require('neogit').setup(opts)
       vim.keymap.set('n', '<leader>gg', "<Cmd>Neogit<CR>", { desc = 'Open Neogit' })
     end
   }
