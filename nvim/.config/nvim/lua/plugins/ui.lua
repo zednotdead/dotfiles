5eb52edf7fc76e8363d28924e690defa12821cd0
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2023-07-05 13:43:03 +0200

    changes

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -4,6 +4,12 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
     config = function()
       require("lualine").setup({
+        options = {
+          disabled_filetypes = {
+            "alpha",
+            "neo-tree"
+          },
+        },
         sections = {
           lualine_c = {
             {
@@ -40,7 +46,7 @@ return {
     init = function() vim.g.barbar_auto_setup = false end,
     opts = {
       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      animation = true,
+      animation = false,
       insert_at_start = true,
       -- …etc.
     },
