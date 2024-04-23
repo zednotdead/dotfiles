5840a7a0e26e16eed6409b1e83d19618a31bd4c7
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@allegro.com>
2024-04-23 11:42:24 +0200

    add neotest

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -282,6 +282,27 @@ return {
 		},
 	},
 	-- }}}
+	-- TESTING & DEBUGGING {{{
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-jest",
+			"rouge8/neotest-rust",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-jest")({}),
+					require("neotest-rust"),
+				},
+			})
+		end,
+	},
+	-- }}}
 	-- MISC BEGIN {{{
 	{
 		"kylechui/nvim-surround",
