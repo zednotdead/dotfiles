d8d66cdc1bd65557b44eb8e3655761c20a476d2b
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2024-09-26 09:02:22 +0200

    modify edgy layout

--- nvim/.config/nvim/lua/plugins/edgy.lua
+++ nvim/.config/nvim/lua/plugins/edgy.lua
@@ -6,21 +6,50 @@ return {
 			vim.opt.laststatus = 3
 			vim.opt.splitkeep = "screen"
 		end,
+		keys = {
+      {
+        "<leader>u", desc = "Window layout"
+      },
+      {
+        "<leader>ue",
+        function()
+          require("edgy").toggle()
+        end,
+        desc = "Edgy Toggle",
+      },
+      -- stylua: ignore
+      { "<leader>uE", function() require("edgy").select() end, desc = "Edgy Select Window" },
+    },
 		---@module "edgy"
 		---@type Edgy.Config
 		opts = {
+			keys = {
+				-- increase width
+				["<c-Right>"] = function(win)
+					win:resize("width", 2)
+				end,
+				-- decrease width
+				["<c-Left>"] = function(win)
+					win:resize("width", -2)
+				end,
+				-- increase height
+				["<c-Up>"] = function(win)
+					win:resize("height", 2)
+				end,
+				-- decrease height
+				["<c-Down>"] = function(win)
+					win:resize("height", -2)
+				end,
+			},
 			bottom = {
-				-- toggleterm / lazyterm at the bottom with a height of 40% of the screen
 				{
 					ft = "toggleterm",
 					size = { height = 0.1 },
-					-- exclude floating windows
-					pinned = true,
-					collapsed = false,
 					---@diagnostic disable-next-line: unused-local
 					filter = function(buf, win)
 						return vim.api.nvim_win_get_config(win).relative == ""
 					end,
+
 				},
 				{ ft = "qf", title = "QuickFix" },
 				{
@@ -35,9 +64,13 @@ return {
 			},
 			left = {
 				{
+					title = "Neo-tree [Filesystem]",
 					ft = "neo-tree",
 					open = function()
 						vim.cmd(("Neotree show filesystem position=%s"):format("left"))
+					end,
+					filter = function(buf)
+						return vim.b[buf].neo_tree_source == "filesystem"
 					end,
 					pinned = true,
 					collapsed = false,
