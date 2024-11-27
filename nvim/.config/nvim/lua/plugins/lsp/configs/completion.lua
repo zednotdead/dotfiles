82c872c04856880283e55e180e2915f3c13beda0
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2024-11-27 13:21:19 +0100

    fix blink.cmp

--- nvim/.config/nvim/lua/plugins/lsp/configs/completion.lua
+++ nvim/.config/nvim/lua/plugins/lsp/configs/completion.lua
@@ -47,28 +47,6 @@ return {
 					min_width = 10, -- max_width controlled by draw-function
 					max_height = 10,
 					cycle = { from_top = false }, -- cycle at bottom, but not at the top
-					draw = function(ctx)
-						-- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/types.lua#L1-L6
-						-- https://github.com/Saghen/blink.cmp/blob/9846c2d2bfdeaa3088c9c0143030524402fffdf9/lua/blink/cmp/windows/autocomplete.lua#L298-L349
-						-- differentiate LSP snippets from user snippets and emmet snippets
-						local source, client = ctx.item.source_id, ctx.item.client_id
-						if client and vim.lsp.get_client_by_id(client).name == "emmet_language_server" then
-							source = "emmet"
-						end
-
-						local sourceIcons = { snippets = "󰩫", buffer = "󰦨", emmet = "" }
-						local icon = sourceIcons[source] or ctx.kind_icon
-
-						return {
-							{
-								" " .. ctx.item.label .. " ",
-								fill = true,
-								hl_group = ctx.deprecated and "BlinkCmpLabelDeprecated" or "BlinkCmpLabel",
-								max_width = 40,
-							},
-							{ icon .. " ", hl_group = "BlinkCmpKind" .. ctx.kind },
-						}
-					end,
 				},
 			},
 			highlight = {
