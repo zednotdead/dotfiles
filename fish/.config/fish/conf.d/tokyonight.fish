f60e135aba6c0e9e6ca57662c3f5710f3f4b55e6
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2024-12-28 22:43:58 +0100

    switch to kanagawa theme

--- fish/.config/fish/conf.d/tokyonight.fish
+++ fish/.config/fish/conf.d/tokyonight.fish
@@ -1,3 +1,3 @@
-if test -n $TOKYONIGHT && status --is-interactive
-    theme_tokyonight $TOKYONIGHT
-end
+# if test -n $TOKYONIGHT && status --is-interactive
+#     theme_tokyonight $TOKYONIGHT
+# end
