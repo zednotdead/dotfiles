da7f4b0f49403f99bd785e6d017c21cfbbf0b6a1
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-05-16 13:37:23 +0200

    add columns

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -10,8 +10,31 @@ return {
 		event = "BufEnter",
 		opts = {
 			enabled = true,
-      ignored_filetypes = { "gitcommit" }
+			ignored_filetypes = { "gitcommit" },
 		},
 	},
 	{ "akinsho/git-conflict.nvim", version = "*", config = true },
+	{
+		"luukvbaal/statuscol.nvim",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"lewis6991/gitsigns.nvim",
+		},
+		config = function()
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				clickhandlers = { -- builtin click handlers, keys are pattern matched
+					Lnum = builtin.lnum_click,
+					FoldClose = builtin.foldclose_click,
+					FoldOpen = builtin.foldopen_click,
+					FoldOther = builtin.foldother_click,
+					DapBreakpointRejected = builtin.toggle_breakpoint,
+					DapBreakpoint = builtin.toggle_breakpoint,
+					DapBreakpointCondition = builtin.toggle_breakpoint,
+					["diagnostic/signs"] = builtin.diagnostic_click,
+					gitsigns = builtin.gitsigns_click,
+				},
+			})
+		end,
+	},
 }
