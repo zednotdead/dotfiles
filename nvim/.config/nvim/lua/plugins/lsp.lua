1a24b24e0ccbe7fc2a2a9377a1e5547bf97d4f7e
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-08-01 09:35:34 +0200

    disable ts_ls and rust-analyzer

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -66,7 +66,14 @@ return {
   },
   {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
+    opts = {
+      automatic_enable = {
+        exclude = {
+          "rust_analyzer",
+          "ts_ls"
+        }
+      }
+    },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
