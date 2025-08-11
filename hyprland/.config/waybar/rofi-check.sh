bd689511d6692e11e3cba81c20b7bbe7495e9df3
Zbigniew Żołnierowicz <zbigniew@zolnierowi.cz>
2025-08-11 11:07:13 +0200

    hyprland

--- hyprland/.config/waybar/rofi-check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-while true; do
-	if pgrep -x "rofi" > /dev/null; then
-		echo '{"class": "active", "text": "Start"}'
-	else
-		echo '{"class": "", "text": "Start"}'
-	fi
-	sleep 0.1
-done
